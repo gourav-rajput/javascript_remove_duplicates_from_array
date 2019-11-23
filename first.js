d648c578cdc5695e47189108d920c08df6c0ba6d
gouravr-94 <gouravr.94@gmail.com>
2019-11-23 13:31:39 +0530

    first function

---
+++ first.js
@@ -0,0 +1,38 @@
+
+const isArray = ele => {
+  return ele && Array.isArray(ele) && ele instanceof Array
+};
+
+const isFunction = ele => {
+  return ele && typeof ele === "function"
+};
+
+isObject = ele => {
+  return ele && typeof ele === 'object' && Array.isArray(ele) === false;
+};
+
+const flatDeepArguments = data => {
+  let dataLength = data.length;
+  let flattenedArray = [];
+  for (let i = 0; i < dataLength; i++) {
+    let currentVal = data[i];
+    if (isArray(currentVal)) {
+      flattenedArray.push(...flatDeepArguments(currentVal));
+    } else if (!isFunction(currentVal) && !isObject(currentVal)) {
+      flattenedArray.push(currentVal);
+    }
+  }
+  return [...flattenedArray];
+};
+
+const removeDuplicates = (...args) => {
+  let flattenedArray = flatDeepArguments(args);
+  let uniqueArray = [];
+  flattenedArray.map(ele => {
+    // Use array indexOf in case of old version of Node
+    if (!uniqueArray.includes(ele)) {
+      uniqueArray.push(ele);
+    }
+  });
+  return [...uniqueArray];
+};
